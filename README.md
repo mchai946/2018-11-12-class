bf070bd5cac190ecd723290269fbd227c15690d2
Maddie Chai <mchai@Maddies-MacBook-Pro-4.local>
2018-11-12 14:28:55 -0500

    Finished class work

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Class Today
 
-Notes
+#Q1
+
+Make a function which takes an Upshot election name and returns the full name of the file.
+ 
+Example, argument is "CA-49-1"
+
+and returns
+
+"elections-polls-ca49-1.csv"
+
+#Q2
+
+Return entire path
+
+#Q3
+
+Return information in the file as a tibble
+
+#Q4
+
+Add some error checking or fixes for stupid users
--- scratch.R
+++ scratch.R
@@ -6,9 +6,14 @@ build_file <- function(x){
   paste("elections-poll-", x, ".csv", sep = "")
 }
 
-#Question 2
+#Question 2/3
 
 download_link <- function(district, wave){
+  
+  district <- tolower(district)
+  
+  stopifnot(is.character(district))
+  
   x <- paste("https://raw.githubusercontent.com/TheUpshot/2018-live-poll-results/master/data/elections-poll-", 
         district, "-", wave, ".csv", sep ="") 
   
